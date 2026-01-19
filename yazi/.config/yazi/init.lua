89771ff0ef8610408c1b283efca21419562e21b9
wendtalexander <wendtalexander@protonmail.com>
2026-01-19 09:37:41 +0100

    [yazi] fixing yazi

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -1,6 +1,5 @@
 -- ~/.config/yazi/init.lua
 function Linemode:size_and_mtime()
-
 	local time = math.floor(self._file.cha.mtime or 0)
 	if time == 0 then
 		time = ""
@@ -11,7 +10,7 @@ function Linemode:size_and_mtime()
 	end
 
 	local size = self._file:size()
-	local perm =self._file.cha:perm() or "" 
+	local perm = self._file.cha:perm() or ""
 
 	return string.format("%s %s %s", size and ya.readable_size(size) or "-", time, perm)
 end
